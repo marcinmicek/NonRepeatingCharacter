aa241b9c0d49de14e8c6c4d857239390f4b352fa
Marcin Micek <marcin.micek@gmail.com>
2023-01-25 23:30:17 +0100

    Added GUI created in Swing

--- src/pl/marcinmicek/NonRepeatingCharacter.java
+++ src/pl/marcinmicek/NonRepeatingCharacter.java
@@ -1,8 +1,48 @@
 package pl.marcinmicek;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 
-class NonRepeatingCharacter {
+public class NonRepeatingCharacter extends JFrame {
+
+    private JTextArea inputTextArea;
+    private JButton findTheCharacterButton;
+    private JTextField resultTextField;
+    private JButton clearButton;
+    private JButton exitButton;
+    private JLabel inputLabel;
+    private JPanel mainPanel;
+    private JLabel resultLabel;
+
+    public NonRepeatingCharacter(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                // clear the text fields
+                inputTextArea.setText("");
+                resultTextField.setText("");
+            }
+        });
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                // exit from application
+                System.exit(0);
+            }
+        });
+        findTheCharacterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resultTextField.setText(SearchingChallenge(inputTextArea.getText()));
+            }
+        });
+    }
 
     public static String SearchingChallenge(String str) {
         // code goes here
@@ -26,10 +66,14 @@ class NonRepeatingCharacter {
     }
 
     public static void main (String[] args) {
-        // keep this function call here
-        System.out.println("Input the string to test:");
-        Scanner s = new Scanner(System.in);
-        System.out.println("The first non-repeating character in the input string is: " + SearchingChallenge(s.nextLine()));
+
+//        System.out.println("Input the string to test:");
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("The first non-repeating character in the input string is: " + SearchingChallenge(s.nextLine()));
+
+        JFrame frame = new NonRepeatingCharacter("Searching for the first non-repeating character in a string");
+        frame.setVisible(true);
+        frame.setSize(700,300);
     }
 
 }
